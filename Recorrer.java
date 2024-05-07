2f1caf729a1ee790cd5cf29d274e6286165a0754
adri999jr <159541783+adri999jr@users.noreply.github.com>
2024-05-07 10:32:33 +0200

    Update Recorrer.java

--- Recorrer.java
+++ Recorrer.java
@@ -3,7 +3,7 @@ package codigo;
 public class Recorrer {
 
 	public void RecorrerPalabra(String palabra) {
-		for(int i=0; i<palabra.length();i++) {
+		for(int i=1; i<palabra.length();i++) {
 			System.out.println(palabra.charAt(i));
 		}
 	}
