13c17a2f35b4e32e738f449f88adf2218fdde264
Adrijr <ajimrod0411@g.educaand.es>
2024-05-07 10:45:13 +0200

    Update Recorrer.java
    hacemos el arreglo

--- Recorrer.java
+++ Recorrer.java
@@ -1,9 +1,9 @@
 package codigo;
 
 public class Recorrer {
-
+// arreglo
 	public void RecorrerPalabra(String palabra) {
-		for(int i=1; i<palabra.length();i++) {
+		for(int i=0; i<palabra.length();i++) {
 			System.out.println(palabra.charAt(i));
 		}
 	}
