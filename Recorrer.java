0cf705cec9272beef39d8d6007fde8b6a6050872
adri999jr <159541783+adri999jr@users.noreply.github.com>
2024-05-07 10:31:03 +0200

    Create Recorrer.java

---
+++ Recorrer.java
@@ -0,0 +1,10 @@
+package codigo;
+
+public class Recorrer {
+
+	public void RecorrerPalabra(String palabra) {
+		for(int i=0; i<palabra.length();i++) {
+			System.out.println(palabra.charAt(i));
+		}
+	}
+}
